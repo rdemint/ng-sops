3612c96bc2feb1e5a16c89b9f473709ab663c73a
raines <rainesdemint@gmail.com>
2019-12-09 12:14:38 -0700

    initial commit.  ng app configured for local Microsoft Add-in development.

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+declare const Office: any;
+
+    Office.initialize = () => {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+        .catch(err => console.log(err));
+    }; 
